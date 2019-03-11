ab940bce512a40882962549cc1e86156362b9a0a
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2019-03-11 12:09:41 +0100

    Add files via upload
    Its'a me, Maori

---
+++ Application/src/MainFrameController.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.effect.*;
+import javafx.scene.text.*;
+import java.lang.*;
+import java.util.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class MainFrameController{
+    //Menu
+
+    //Mine Økter
+
+    //Ny Økt
+
+    //Mine Øvelser
+
+    //Mine Apparater
+
+    //Mine Øvelsegrupper
+
+    //About
+
+}
